69959f0534fc2dd3f2e42575e2f07bc68bda346e
Aung Myo Linn <aunggyijava@gmail.com>
2024-11-17 21:35:00 +0630

    initially commit

---
+++ auth-server/src/main/java/com/safalifter/authservice/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package com.safalifter.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+//@EnableEurekaClient
+@EnableDiscoveryClient
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
